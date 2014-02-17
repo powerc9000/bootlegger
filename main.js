1646183dfa9e349ab3730e46e8b796b30c76d302
powerc9000 <clay.murray8@gmail.com>
2014-02-16 23:01:13 -0700

    starting to add masking for lighting

--- main.js
+++ main.js
@@ -95,6 +95,7 @@ var $h = require("./head-on"),
 	NPC = require("./NPC"),
 	init = require("./init"),
 	camera = new $h.Camera(500, 500),
+	maskCamera = new $h.Camera(500,500),
 	keyMap = {
 		37:"left",
 		38:"up",
@@ -102,6 +103,7 @@ var $h = require("./head-on"),
 		40:"down",
 		32: "space"
 	},
+	mask,
 	canvas,
 	player,
 	npc;
@@ -113,22 +115,33 @@ $h.gamestate = {};
 $h.gamestate.camera = camera;
 $h.gamestate.player = player;
 $h.canvas.create("main", 500, 500, camera);
+$h.canvas.create("mask", 500, 500, maskCamera);
+mask = $h.canvas("mask");
 canvas = $h.canvas("main");
-canvas.append("body"); 
+canvas.append("body");
+mask.append("body");
 $h.keys = {};
+console.log(mask);
+mask.drawRect( 500, 500,0,0, "rgba(0,0,0,0.9)");
+mask.canvas.ctx.globalCompositeOperation = 'destination-out';
 $h.render(function(){
 	canvas.clear();
+	mask.drawRect(50,50,250,250,"white");
+
+
 	for(var i=0; i<200; i++){
 		canvas.drawImage($h.images("road"), 200, i*128);
 	}
 	player.render(canvas);
 	npc.render(canvas);
 	canvas.drawRect(20,20, 20, 20, "green");
+	canvas.canvas.ctx.drawImage(mask.canvas.canvas,0,0);
 });
 
 $h.update(function(delta){
 	player.update(delta);
 	npc.update(delta);
+
 });
 $h.run();
 canvas.canvas.canvas.addEventListener("mousemove", function(e){
